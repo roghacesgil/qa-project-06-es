30effc6a14b3f9a1e1aec6a061ca9272141547f4
Rogelio Triple Ten <rogeliotripleten@MacBook-Air-de-Rogelio-2.local>
2024-03-03 22:16:31 -0600

    URL actualizada

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-c8986c47-c5bb-4c7c-88de-d7158b41c2f6.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-6da7cc16-8fea-491f-80e6-0f3d569fd4b1.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
-KITS_PATH = "/api/v1/kits
+KITS_PATH = "/api/v1/kits"
